6904fe32f2764dddc7ba979f049311dad2294c8d
Diego Liascovich <padie78@gmail.com>
2025-07-01 23:44:12 +0300

    adding main menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +7,22 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+
+  constructor(private router: Router){}
+
+  selectedItem: string | null = null;
+  select(item: string) {
+    this.selectedItem = item;
+    console.log('Seleccionó:', item);
+    // Aquí podés hacer la acción: abrir modal, cargar datos, etc.
+    if (item === 'order') {
+        this.router.navigate(['/orders']); // o la ruta que hayas definido
+    } else if (item === 'payment') {
+        this.router.navigate(['/payments']);
+    }
+  }
+}
+
+
 
